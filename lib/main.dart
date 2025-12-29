366077101593f25044049539c243a71960014728
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-12-29 08:00:10 +0700

    Enhance Todo functionality with error handling and loading states

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:math';
 
-import 'package:riverpod_test/main_sync.dart';
-
 void main() {
   runApp(
     // For widgets to be able to read providers, we need to wrap the entire application in a "ProviderScope" widget. This is where the state of our providers will be stored.
-    ProviderScope(child: MyApp()),
+    ProviderScope(
+      child: MyApp(),
+      // Customize the error handling behavior of providers in this scope.
+      // Here, we specify that if a provider fails, it should not retry.
+      retry: (retryCount, error) => null,
+    ),
   );
 }
 
@@ -25,31 +28,93 @@ class TodoData {
 final totoFutureProvider = FutureProvider<List<TodoData>>(
   (ref) async {
     await Future.delayed(Duration(seconds: 1));
-    return <TodoData>[
-      TodoData(id: 0, title: 'Todo 0'),
-      TodoData(id: 1, title: 'Todo 1'),
-      TodoData(id: 2, title: 'Todo 2'),
-    ];
+
+    double rand = Random().nextDouble();
+    // Simulate a success or failure based on random value
+    if (rand > 0.5) {
+      return <TodoData>[
+        TodoData(id: 0, title: 'Todo 0'),
+        TodoData(id: 1, title: 'Todo 1'),
+        TodoData(id: 2, title: 'Todo 2'),
+      ];
+    } else {
+      throw Exception("Something went wrong!");
+    }
   },
 );
 
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("My Todo (Async)")),
+        body: TodoDisplayHandler(),
+      ),
+    );
+  }
+}
+
+class TodoDisplayHandler extends ConsumerWidget {
+  const TodoDisplayHandler({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final todos = ref.watch(totoFutureProvider);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("My Todo (Async)")),
-        body: Column(
-          children: [
-            Row(
-              children: [],
-            ),
-          ],
-        ),
+    return todos.when(
+      data: (data) => TodoDisplay(todos: data),
+      error: (error, stackTrace) => ErrorDisplay(error: error),
+      loading: () => LoadingDisplay(),
+    );
+  }
+}
+
+class TodoDisplay extends StatelessWidget {
+  const TodoDisplay({super.key, required this.todos});
+
+  final List<TodoData> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: .start,
+        crossAxisAlignment: .start,
+        spacing: 10,
+        children: todos
+            .map(
+              (todo) => Row(
+                mainAxisSize: .min,
+                spacing: 10,
+                children: [
+                  Text(todo.title),
+                ],
+              ),
+            )
+            .toList(),
       ),
     );
+  }
+}
+
+class LoadingDisplay extends StatelessWidget {
+  const LoadingDisplay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const CircularProgressIndicator());
+  }
+}
+
+class ErrorDisplay extends StatelessWidget {
+  const ErrorDisplay({super.key, this.error});
+
+  final Object? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("An error occurred: ${error.toString()}"));
   }
 }
